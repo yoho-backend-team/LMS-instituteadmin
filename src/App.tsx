c50530d72178fdb1477188347d0307fbd67c4e0d
Vigneshwari3108 <vigneshwari.yoho@gmail.com>
2025-07-16 18:38:55 +0530

    branches

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button"
+import BranchPage from "./components/Branch/branchPage"
+import BranchOverview from "./components/Branch/branchOverview"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <>
+    <BranchPage/>
+    <BranchOverview/>
+    </>
+    
   )
 }
 
