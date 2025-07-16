f2092e163edb3f1aa89b73e67c4b29ac6c7bd029
nivetha <nivethayoho@gmail.com>
2025-07-16 19:31:39 +0530

    Dashboard admin user page creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
+import Dashboard from "./components/ui/User/Dashboard"
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+      {/* <Button>Click me</Button> */}
+      <Dashboard />
     </div>
   )
 }
