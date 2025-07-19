71b25a1f2aef3ae22f20e181e68b6115243621f8
nivetha <nivethayoho@gmail.com>
2025-07-19 19:27:49 +0530

    changed content

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import SalaryDetails from "./components/ui/Salary/SalaryDetails"
 
 // import SalaryTable from "./components/ui/Salary/SalaryTable"
 
-// import Dashboard from "./components/ui/User/Dashboard"
+import Dashboard from "./components/ui/User/Dashboard"
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       {/* <Button>Click me</Button> */}
-      {/* <Dashboard /> */}
-    <SalaryDetails />
+      <Dashboard />
+    {/* <SalaryDetails /> */}
     </div>
   )
 }
